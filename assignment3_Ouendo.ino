e3908c102e0274bb22b0e20e7473ef82d2c8976d
Pe-e <59469872+Pe-e@users.noreply.github.com>
2022-04-15 20:10:51 +0100

    update semaphores

--- assignment3_Ouendo.ino
+++ assignment3_Ouendo.ino
@@ -45,7 +45,7 @@ struct dataTasks{
 };
 
 QueueHandle_t errorQueue = xQueueCreate(1,sizeof(int));
-SemaphoreHandle_t xSerialSemaphore;
+SemaphoreHandle_t mutex;
 int analogs[4] = {0,0,0,0};
 int count = 0;
 struct dataTasks results;
@@ -60,13 +60,10 @@ void setup() {
   
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
+  mutex = xSemaphoreCreateMutex(); // initialisation of the semaphore as mutual exclusion, so the initial value is 1
+  
 
-  if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
-  {
-    xSerialSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
-    if ( ( xSerialSemaphore ) != NULL )
-      xSemaphoreGive( ( xSerialSemaphore ) ); 
-  }
+  
 
   
   // Now set up TASKS.
@@ -177,8 +174,9 @@ void task2(void *pvParameters)  // This is a task.
   for (;;)
   {
     //Serial.println("Task2");
-    
-   results.buttonState = digitalRead(BUTTON_1);  //RETURN STATE OF THE BUTTON
+   xSemaphoreTake(mutex, portMAX_DELAY);
+   results.buttonState = digitalRead(BUTTON_1); //STATE OF THE BUTTON
+   xSemaphoreGive(mutex);
    vTaskDelay(task2_period);  
    
   }
@@ -194,7 +192,9 @@ void task3(void *pvParameters)  // This is a task.
     int lowInput = pulseIn(SQUARE_INPUT,LOW);     //LOW PULSE PERIOD uS
     int totalInput = highInput + lowInput; //WHOLE SIGNAL PERIOD
     int frequency=1000000/totalInput;  // CALCUL FREQUENCY IN HZ (1/PERIOD(uS)) * 1000000
+    xSemaphoreTake(mutex, portMAX_DELAY);
     results.freqResult = frequency;
+    xSemaphoreGive(mutex); 
     vTaskDelay(task3_period);
   }
 }
@@ -204,7 +204,9 @@ void task4(void *pvParameters)  // This is a task.
   (void) pvParameters;
   for (;;){
   //  Serial.println("Task4");
+    xSemaphoreTake(mutex, portMAX_DELAY);
     results.analogValue = analogRead(DC_INPUT); //READ POTENTIOMETER ANALOG VALUE
+    xSemaphoreGive(mutex); 
     vTaskDelay(task4_period); 
   }
 }
@@ -220,17 +222,17 @@ void task5(void *pvParameters)  // This is a task.
     int *ptrSend;
   //  Serial.println("Task5");
     int sum = 0;
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE ){
-      if(count == 0 || count % 4 == 0) //UPDATE VALUES IN THE ARRAY 
-        analogs[0] = results.analogValue;
-      else if(count == 1 || count % 4 == 1) //UPDATE VALUES IN THE ARRAY 
-        analogs[1] = results.analogValue;
-      else if(count == 2 || count % 4 == 2) //UPDATE VALUES IN THE ARRAY 
-        analogs[2] = results.analogValue;
-      else
-        analogs[3] = results.analogValue;
-      xSemaphoreGive( xSerialSemaphore );
-    }
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    if(count == 0 || count % 4 == 0) //UPDATE VALUES IN THE ARRAY 
+      analogs[0] = results.analogValue;
+    else if(count == 1 || count % 4 == 1) //UPDATE VALUES IN THE ARRAY 
+      analogs[1] = results.analogValue;
+    else if(count == 2 || count % 4 == 2) //UPDATE VALUES IN THE ARRAY 
+      analogs[2] = results.analogValue;
+    else
+      analogs[3] = results.analogValue;
+    xSemaphoreGive(mutex);
+    
     //Serial.println(count);
    // Serial.println("0:"+String(analogs[0])+"\n,1:"+String(analogs[1])+"\n,2:"+String(analogs[2])+"\n,3:"+String(analogs[3]));
     count++;  // COUNT THE NUMBER OF VALUE ANALOG RETURNED TO GET THE 4 LAST VALUES WITH MODULO 4 CONDITION
@@ -242,16 +244,12 @@ void task5(void *pvParameters)  // This is a task.
     if(count<4)
       results.analogAvg = sum/count;    //CALCULATE AVG IF NUMBER OF VALUE IN THE ARRAY IS UNDER 4
     else
-      results.analogAvg =  sum/4;
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE ){//CALCULATE AVG IN THE ARRAY
-      ptrSend = &results.analogAvg;
-      xSemaphoreGive( xSerialSemaphore );
+      results.analogAvg =  sum/4;      //CALCULATE AVG IN THE ARRAY
       
-    }
-    //if(xQueueSend( errorQueue, ptrSend, portMAX_DELAY ) != "pdPass"){
-     // Serial.println("nopass");
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    ptrSend = &results.analogAvg;
+    xSemaphoreGive( mutex );
     xQueueOverwrite( errorQueue, ptrSend ); 
-    //}
     vTaskDelay(task5_period);
     
   }
@@ -288,7 +286,9 @@ void task7(void *pvParameters)  // This is a task.
     int error=0;
     if( ptrReceive > 4095/2)
       error = 1;
+    xSemaphoreTake(mutex, portMAX_DELAY);
     results.errorCode = error;
+    xSemaphoreGive(mutex);
     vTaskDelay(task7_period);
   }
 }
@@ -299,11 +299,9 @@ void task8(void *pvParameters)  // This is a task.
   for (;;)
   {
    // Serial.println("Task8");
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE ){
-      digitalWrite(RED, results.errorCode);
-      xSemaphoreGive( xSerialSemaphore );
-    }
-    //IF ERROR LED SWITCH ON
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    digitalWrite(RED, results.errorCode);     //IF ERROR LED SWITCH ON
+    xSemaphoreGive( mutex );
     vTaskDelay(task8_period);
         
   }
@@ -313,23 +311,23 @@ void task9(void *pvParameters)  // This is a task.
   (void) pvParameters;
   for (;;)
   {
-      //Serial.println("Task9");
+      Serial.println("Task9");
   
     if( results.buttonState == HIGH){
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
+      xSemaphoreTake(mutex, portMAX_DELAY);
+ 
       // We were able to obtain or "Take" the semaphore and can now access the shared resource.
       // We want to have the Serial Port for us alone, as it takes some time to print,
       // so we don't want it getting stolen during the middle of a conversion.
       // print out the state of the button:
-        Serial.println( //PRINT RESULTS
-          "bs:"+String(results.buttonState)+
-          ",F:"+ String(results.freqResult)+ 
-          ",analog:"+String(results.analogValue)+ 
-          ",avg:"+String(results.analogAvg)+ 
-          ",error:"+String(results.errorCode));
-          xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
-       }
+      Serial.println( //PRINT RESULTS
+        "bs:"+String(results.buttonState)+
+        ",F:"+ String(results.freqResult)+ 
+        ",analog:"+String(results.analogValue)+ 
+        ",avg:"+String(results.analogAvg)+ 
+        ",error:"+String(results.errorCode));
+        xSemaphoreGive(mutex); // Now free or "Give" the Serial Port for others.
+     
       
     }
     vTaskDelay(task9_period);
